74be3d0620e29e3c24977d437286c6781ce4c54b
Seongho Park <bluetosh7@gmail.com>
2018-05-26 22:31:02 +0900

    feat: Navbar 추가

---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .page-search
+    navbar
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  components: {
+    navbar: Navbar,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+</style>
