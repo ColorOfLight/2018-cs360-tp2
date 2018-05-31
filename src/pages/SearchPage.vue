3829f8e57e648c439ce91af56e34629efeb93c1d
Seongho Park <bluetosh7@gmail.com>
2018-05-31 18:28:48 +0900

    feat: add store detail card

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -29,8 +29,38 @@
                 span.recent Recent
                 .text 교내배달 되긴한데 한마리 세트는 안해주시고 두마리세트 부터 해주시는 거 같아요... 좀 너무한 거 같긴 하네요...
           .col-6
-            b-card.empty-card(no-body)
+            b-card.empty-card(no-body v-if="false")
               b-card-body Please select a store.
+            b-card.store-detail-card(no-body)
+              b-card-body.detail-title-body
+                i.material-icons.icon-favorite.active star
+                .title 에꿍이치킨 (신성점)
+              b-card-body.detail-content-body
+                b-card(no-body)
+                  b-card-body.ratings-body
+                    .title Ratings
+                    .ratings-content-wrapper
+                      .rating 80%
+                      .rating-text positive
+                      .rating-graph-wrapper
+                        .rating-graph
+                          .rating-graph-fill(style="width:80%;")
+                        .left-text 96
+                        .right-text 24
+                  b-card-body.tags-body
+                    .title Tags
+                    .tags-container
+                      .tag #치킨
+                      .tag #양많음
+                      .tag #교내배달
+                      .tag #건강
+                b-card(no-body)
+                  b-card-body.reviews-title-body
+                    .title Reviews
+                  b-card-body.reviews-content-body
+                    .text 교내배달 되긴한데 한마리 세트는 안해주시고 두마리세트 부터 해주십니다
+                  b-card-body.reviews-content-body
+                    .text 교내배달 되긴한데 한마리 세트는 안해주시고 두마리세트 부터 해주십니다
 </template>
 
 <script>
@@ -44,6 +74,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$store-detail-title-height: 4rem;
+
+.icon-favorite {
+  color: $gray-300;
+
+  &.primary {
+    color: $primary;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
 .navbar {
   z-index: $navbar-z;
 }
@@ -78,7 +122,6 @@ export default {
     }
 
     .icon-favorite {
-      color: $primary;
       font-size: 1rem;
       margin-right: .75rem;
     }
@@ -103,7 +146,6 @@ export default {
   }
 
   .icon-favorite {
-    color: $gray-300;
     left: 1rem;
     position: absolute;
     top: 1.4rem;
@@ -160,6 +202,127 @@ export default {
     .text {
       @include text-truncate;
       padding-left: 4.25rem;
+    }
+  }
+}
+
+.store-detail-card {
+  .detail-title-body {
+    border-bottom: 1px solid $gray-300;
+    height: $store-detail-title-height;
+    padding: 0;
+    position: relative;
+
+    .icon-favorite {
+      left: 1.25rem;
+      position: absolute;
+      top: 1.25rem;
+    }
+
+    .title {
+      font-size: 1.25rem;
+      margin-left: 4rem;
+      line-height: $store-detail-title-height;
+    }
+  }
+
+  .detail-content-body {
+    background-color: $gray-50;
+    padding: 1rem;
+
+    .card {
+      margin-bottom: 2rem;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    .card-body {
+      padding: 1rem 1.25rem;
+    }
+
+    .title {
+      color: $gray-700;
+    }
+
+    .ratings-body {
+      border-bottom: 1px solid $gray-300;
+    }
+
+    .ratings-content-wrapper {
+      margin-top: .25rem;
+      position: relative;
+    }
+
+    .rating {
+      color: $primary;
+      display: inline-block;
+      font-size: 2.25rem;
+    }
+
+    .rating-text {
+      color: $gray-500;
+      display: inline-block;
+      margin-left: .5rem;
+    }
+
+    .rating-graph-wrapper {
+      left: 11.25rem;
+      position: absolute;
+      right: 0;
+      top: .75rem;
+
+      .left-text {
+        color: $primary;
+        display: inline-block;
+      }
+
+      .right-text {
+        color: $gray-500;
+        float: right;
+      }
+    }
+
+    .rating-graph {
+      background-color: $gray-300;
+      height: 0.25rem;
+      margin-bottom: .25rem;
+      position: relative;
+      width: 100%;
+    }
+
+    .rating-graph-fill {
+      background-color: $primary;
+      height: 100%;
+    }
+
+    .tags-container {
+      margin-top: .75rem;
+
+      .tag {
+        color: $primary;
+        display: inline-block;
+        margin-right: .5rem;
+        text-decoration: underline;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+  }
+
+  .reviews-title-body {
+    border-bottom: 1px solid $gray-300;
+    color: $gray-700;
+  }
+
+  .reviews-content-body {
+    border-bottom: 1px solid $gray-300;
+
+    &:last-child {
+      border-bottom: 0;
     }
   }
 }
