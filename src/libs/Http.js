5241af8ec23026f14589b499feca92ce72dae979
Seongho Park <bluetosh7@gmail.com>
2018-06-05 02:27:17 +0900

    feat: add favorites features

--- src/libs/Http.js
+++ src/libs/Http.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.delete['Content-Type'] = 'application/x-www-form-urlencoded';
 
 const axiosInstance = axios.create({
   baseURL: "http://13.125.127.34:3000",
